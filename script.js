d1f7aadce73df47a047e83b6f4ac93b4bb5302a2
jusouzacrisouza <Vinicius_r_soares@hotmail.com>
2025-01-21 21:12:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,6 +32,29 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', () => {
   alert('Vou te contar um pedacinho da minha história!');
   // Adicione aqui a ação que você deseja que o botão execute
-  // Por exemplo, redirecionar para outra página:
   // window.location.href = 'https://jusouzacris.github.io/projetosinicais.com/';
+  const agora = new Date();
+  const hora = agora.getHours();
+  let mensagem;
+
+  if (hora >= 5 && hora < 12) {
+    mensagem = "Bom dia!";
+  } else if (hora >= 12 && hora < 18) {
+    mensagem = "Boa tarde!";
+  } else {
+    mensagem = "Boa noite!";
+  }
+
+  const formatador = new Intl.DateTimeFormat('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+  const dataFormatada = formatador.format(agora);
+
+  alert(mensagem + " É ótimo te ver aqui! Assinado: Jú\nData e hora: " + dataFormatada);
+  window.location.href = 'https://jusouzacris.github.io/projetosinicais.com/';
 });
